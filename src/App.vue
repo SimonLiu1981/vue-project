ffa0242f1051529889317dbfa9673df16e0f558d
SimonLiu1981 <sgci@163.com>
2025-07-14 17:59:10 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  
+  <HelloWorld msg="Welcome Simon test"/>
+</template>
+
+<script>
+ 
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    call() {
+      create1()
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template> 
+  <h3>{{msg}}</h3>
+  <button @click="callme()">You clicked me {{ count }} times.</button>
+</template>
+
+<script>
+
+import 'datejs';
+
+export default  {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count:0
+    }
+  },
+  methods: {
+    callme() {
+      var d = new Date();
+      
+      alert(d.toString("dd/MM"))
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
